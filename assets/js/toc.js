38b497dbde4f6dc80b849789475b31fd00b9241d
Scott Hurrey <Scott.Hurrey@blackboard.com>
2020-11-25 16:26:27 -0500

    Update toc.js to distributed version

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,11 +1,13 @@
-import { find, findOne, ready, on, fire } from 'domassist';
+import { find, findOne, on, fire, ready } from 'domassist';
 import scrollTriggers from 'scroll-triggers';
-import { init as smoothScroll, scroll } from 'smooth-scroller';
+import { init as init$1, scroll } from 'smooth-scroller';
 
 function init(el) {
   if (!el) {
     el = find('[data-toc]');
-    el.forEach(e => init(e));
+    el.forEach(function (e) {
+      return init(e);
+    });
     return;
   }
 
@@ -13,84 +15,79 @@ function init(el) {
     return;
   }
 
-  const container = el.dataset.tocContainer ?
-    findOne(el.dataset.tocContainer) || document.body : document.body;
-  const selectors = el.dataset.toc.split(',').map(s => s.trim());
-  const tocItems = [];
-  const offset = el.dataset.tocOffset ? parseInt(el.dataset.tocOffset, 10) : 1;
-  let i = 1;
+  var container = el.dataset.tocContainer ? findOne(el.dataset.tocContainer) || document.body : document.body;
+  var selectors = el.dataset.toc.split(',').map(function (s) {
+    return s.trim();
+  });
+  var tocItems = [];
+  var offset = el.dataset.tocOffset ? parseInt(el.dataset.tocOffset, 10) : 1;
+  var i = 1; // Building dict
 
-  // Building dict
-  selectors.forEach(selector => {
-    const items = find(selector, container);
-
-    items.forEach(item => {
+  selectors.forEach(function (selector) {
+    var items = find(selector, container);
+    items.forEach(function (item) {
       // Keep the id if already there
-      const index = item.id || `toc-${i++}`;
-      const text = item.dataset.tocTitle ?
-      item.dataset.tocTitle.trim() : item.textContent.trim();
-      const sanitizedClassName = selector.replace(/((:+[\w-\d]*)|[^A-z0-9-\s])/g, ' ').replace(/\s{2,}/g, ' ').trim();
-      const className = `toc-${sanitizedClassName}`;
+      var index = item.id || "toc-" + i++;
+      var text = item.dataset.tocTitle ? item.dataset.tocTitle.trim() : item.textContent.trim();
+      var sanitizedClassName = selector.replace(/((:+[\w-\d]*)|[^A-z0-9-\s])/g, ' ').replace(/\s{2,}/g, ' ').trim();
+      var className = "toc-" + sanitizedClassName; // Set it if none
 
-      // Set it if none
       if (item.id !== index) {
         item.id = index;
       }
 
-      tocItems.push({ index, text, className });
+      tocItems.push({
+        index: index,
+        text: text,
+        className: className
+      });
     });
   });
+  var html = '<ul>';
+  var triggerOptions = []; // Building markup
 
-  let html = '<ul>';
-  const triggerOptions = [];
-
-  // Building markup
-  tocItems.forEach((item, j) => {
-    const nextEl = tocItems[j + 1];
-    const options = {
-      el: `.toc-li-${j}`,
+  tocItems.forEach(function (item, j) {
+    var nextEl = tocItems[j + 1];
+    var options = {
+      el: ".toc-li-" + j,
       fixed: 'true',
-      start: `#${item.index}`,
+      start: "#" + item.index,
       position: 'top',
       positionEnd: 'top',
       className: 'toc-visible'
     };
-    html += `\n<li class="toc-li-${j} ${item.className}"><a href="#${item.index}">${item.text}</a></li>`;
+    html += "\n<li class=\"toc-li-" + j + " " + item.className + "\"><a href=\"#" + item.index + "\">" + item.text + "</a></li>";
 
     if (nextEl) {
-      options.end = `#${nextEl.index}`;
+      options.end = "#" + nextEl.index;
     }
 
     triggerOptions.push(options);
   });
-
   html += '</ul>';
-
   el.innerHTML = html;
-  const tocs = find('li', el);
-  const anchors = find('a', el);
+  var tocs = find('li', el);
+  var anchors = find('a', el); // Setting up scroll triggers and smooth scroll
 
-  // Setting up scroll triggers and smooth scroll
   scrollTriggers(triggerOptions);
-  smoothScroll(anchors, offset);
+  init$1(anchors, offset); // Pause scroll triggers while smoothscrolling
 
-  // Pause scroll triggers while smoothscrolling
-  on(document.body, 'smoothscroll:start', () => {
+  on(document.body, 'smoothscroll:start', function () {
     fire(tocs, 'scrolltriggers:pause');
   });
-
-  on(document.body, 'smoothscroll:end', () => {
+  on(document.body, 'smoothscroll:end', function () {
     fire(tocs, 'scrolltriggers:resume');
     fire(window, 'scroll');
   });
 
   if (window.location.hash) {
-    anchors.some(anchor => {
-      const found = anchor.getAttribute('href') === window.location.hash;
+    anchors.some(function (anchor) {
+      var found = anchor.getAttribute('href') === window.location.hash;
 
       if (found) {
-        setTimeout(() => {
-          const element = findOne(window.location.hash);
+        setTimeout(function () {
+          var element = findOne(window.location.hash);
+
           if (element) {
             // Silent scroll to element
             scroll(element, null, offset, true);
@@ -102,7 +99,7 @@ function init(el) {
     });
   }
 }
+ready(init);
 
 export default init;
-
-ready(init);
+//# sourceMappingURL=toc.js.map
